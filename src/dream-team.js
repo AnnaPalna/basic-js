5dd1b40f54d322364c1056e9c7cfe0d8e643f992
Anna Ibraeva <ibraeva.ann@gmail.com>
2021-09-15 17:48:41 +0300

    feat^ add resolve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let res =  members
+    .filter(member => (typeof member === 'string'))
+    .map(member => member.trim().toUpperCase().slice(0,1))
+
+  
+  return res.sort().join('')
+  
 }
