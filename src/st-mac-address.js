02b97ed0edb04ed51b76028fca4893be96a494f7
Anna Ibraeva <ibraeva.ann@gmail.com>
2021-09-17 15:31:56 +0300

    feat: resolve mac address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  const arr = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  let items = n.split('-')
+  console.log(items)
+
+ for (let item of items) {
+    item = item.split('')
+    console.log(item)
+    for (const item0 of item) {
+      if (!arr.includes(item0)) {
+        return false
+      }
+    } 
+  }
+return true 
 }
