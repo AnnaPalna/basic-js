f7add35d5ebf64c2a9465ceb0ade0138e385f0d8
Anna Ibraeva <ibraeva.ann@gmail.com>
2021-09-17 14:59:44 +0300

    feat: resolve encode line

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,8 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+  calculateDepth(/* sarr */) {
+    
     // remove line with error and write your code here
   }
 }
--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str.length === 0) {
+    return str
+  }
+
+  let res = ''
+  let num = 1
+  
+  for (let i = 0; i < str.length; i++) {
+  	if (str[i] === str[i+1]) {
+    	num++
+    }
+  
+    else {
+    
+      if (num > 1) {
+    	res += `${num}${str[i]}`
+    }
+      if (num === 1) {
+      res += `${str[i]}`
+    }
+          num = 1
+  }
+}
+  
+  return res
+  
 }
