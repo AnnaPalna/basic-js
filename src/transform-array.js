312a1132c48e4c3d7c446fca761ef03a4ce58565
Anna Ibraeva <ibraeva.ann@gmail.com>
2021-09-18 16:05:14 +0300

    feat: partly resolve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,49 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+
+  if (arr instanceof Array === false) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  let copy = arr.slice(0)
+  let res = []
+
+  for (let i = 0; i < copy.length; i++) {
+    if (copy[i] === '--discard-next') {
+      if (copy[i] !== undefined) {
+        copy.splice(i+1, 1)
+        copy.splice(i, 1)
+      }
+    }
+
+    if (copy[i] === '--discard-prev') {
+      if (copy[i - 1] !== undefined) {
+        copy.splice(i, 1)
+        copy.splice(i - 1, 1)
+      } else {
+        copy.splice(i, 1)
+      }
+    }
+
+    if (copy[i] === '--double-next') {
+      if (copy[i + 1] !== undefined) {
+        copy.splice(i, 1, copy[i + 1])
+      } else {
+        copy.splice(i, 1)
+      }
+    }
+
+    if (copy[i] === '--double-prev') {
+      if (copy[i - 1] !== undefined) {
+        copy.splice(i, 1, copy[i - 1])
+      } else {
+        copy.splice(i, 1)
+      }
+    }
+  }
+
+  return copy
+
 }
